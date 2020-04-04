eaa50e1790376bcfd8311adfc52f226eba8b075b
Celeste Cassel de Camps <celestec@rogers.com>
2020-04-04 18:58:57 -0400

    read in data

---
+++ car-accidents.R
@@ -0,0 +1,8 @@
+library(gurobi)
+
+############################################ DATA PREPARATION ############################################
+
+# setwd("")
+setwd("/Users/Celeste/Documents/GitHub/car-accidents/")
+data=read.csv("NCDB_1999_to_2014.csv")
+                            
