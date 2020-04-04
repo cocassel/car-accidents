962b41d09db985ae740491a040c2ebe5464ed0aa
a2dakhqa <43449620+a2dakhqa@users.noreply.github.com>
2020-04-04 19:24:42 -0400

    Add Ahmad setwd statement

--- car-accidents.R
+++ car-accidents.R
@@ -2,7 +2,7 @@ library(gurobi)
 
 ############################################ DATA PREPARATION ############################################
 
-# setwd("")
+#setwd("/Users/Ahmad Dakhqan/Desktop/MSCI 433/Project/NCDB_1999_to_2014.csv")
 setwd("/Users/Celeste/Documents/GitHub/car-accidents/")
 data=read.csv("NCDB_1999_to_2014.csv")
                             
