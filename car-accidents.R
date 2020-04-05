2deea98da248481cd4a54fc8acd655950207b1ab
a2dakhqa <43449620+a2dakhqa@users.noreply.github.com>
2020-04-04 21:00:05 -0400

    Update car-accidents.R

--- car-accidents.R
+++ car-accidents.R
@@ -1,8 +1,16 @@
+#install.packages("caTools")
+library(caTools) 
 library(gurobi)
 
 ############################################ DATA PREPARATION ############################################
 
-#setwd("/Users/Ahmad Dakhqan/Desktop/MSCI 433/Project/NCDB_1999_to_2014.csv")
-setwd("/Users/Celeste/Documents/GitHub/car-accidents/")
+setwd("/Users/Ahmad Dakhqan/Desktop/MSCI 433/Project/NCDB_1999_to_2014.csv")
+#setwd("/Users/Celeste/Documents/GitHub/car-accidents/")
 data=read.csv("NCDB_1999_to_2014.csv")
-                            
+
+                         
+split = sample.split(data$C_SEV, SplitRatio = 0.7)
+dataTrain = subset(data, split == TRUE)   # Observations to be put in the training set           
+dataTest = subset(data, split == FALSE)  # Observations to be put in the testing set 
+nrow(dataTrain)
+nrow(dataTest)
