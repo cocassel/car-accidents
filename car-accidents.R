f44bdab7608973b3a0cca0076fe802e428baf7c4
a2dakhqa <43449620+a2dakhqa@users.noreply.github.com>
2020-04-05 21:49:40 -0400

    Cleanned all columns

--- car-accidents.R
+++ car-accidents.R
@@ -1,3 +1,4 @@
+#rm(list=ls())
 #install.packages("caTools")
 library(caTools) 
 library(gurobi)
@@ -12,7 +13,75 @@ data=read.csv("data.csv")
 data$C_SEV[data$C_SEV == 2] = 0
 
 nrow(data)
-data<-data[!(data$C_MNTH=="UU"),] #data cleaning 
+data<-data[!(data$C_MNTH=="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_WDAY =="U"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_HOUR=="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_VEHS =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_CONF =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_CONF =="QQ"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_RCFG =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_RCFG =="QQ"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_WTHR =="U"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_WTHR =="Q"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_RSUR =="U"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_RSUR =="Q"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_RALN =="U"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_RALN =="Q"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_TRAF =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$C_TRAF =="QQ"),] #data cleaning
+nrow(data)
+data<-data[!(data$V_ID =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$V_TYPE =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$V_TYPE =="QQ"),] #data cleaning
+nrow(data)
+data<-data[!(data$V_TYPE =="NN"),] #data cleaning
+nrow(data)
+data<-data[!(data$V_YEAR =="UUUU"),] #data cleaning
+nrow(data)
+data<-data[!(data$V_YEAR =="NNNN"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_ID =="NN"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_AGE =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_AGE =="NN"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_PSN =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_PSN =="QQ"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_PSN =="NN"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_ISEV =="U"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_ISEV =="N"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_SAFE =="UU"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_SAFE =="QQ"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_SAFE =="NN"),] #data cleaning
+nrow(data)
+data<-data[!(data$P_USER =="U"),] #data cleaning
+nrow(data)
+
 
 split = sample.split(data$C_SEV, SplitRatio = 0.7)
 dataTrain = subset(data, split == TRUE)   # Observations to be put in the training set           
