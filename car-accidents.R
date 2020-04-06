2c3f02780e4d74c2de5ca16f2419298c7346f798
Celeste Cassel de Camps <celestec@rogers.com>
2020-04-05 21:29:02 -0400

    Update car-accidents.R

--- car-accidents.R
+++ car-accidents.R
@@ -11,11 +11,13 @@ data=read.csv("data.csv")
 # Replace 2s with 0s. End up with 1s as fatalities and 0s as non-fatal
 data$C_SEV[data$C_SEV == 2] = 0
 
+data = subset(data, P_SEX!="U")
+
 split = sample.split(data$C_SEV, SplitRatio = 0.7)
 dataTrain = subset(data, split == TRUE)   # Observations to be put in the training set           
 dataTest = subset(data, split == FALSE)  # Observations to be put in the testing set 
 nrow(dataTrain)
 nrow(dataTest)
 
-severeLog = glm(C_SEV ~ P_AGE + P_SEX, data = dataTrain, family = binomial) 
+severeLog = glm(C_SEV ~ P_AGE + P_SEX, data = dataTrain, family = binomial(link = "logit")) 
 summary(severeLog)
