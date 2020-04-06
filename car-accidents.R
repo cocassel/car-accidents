481318a463abf1a90123c4d4ce85af536b6dedb6
Celeste Cassel de Camps <celestec@rogers.com>
2020-04-05 20:41:01 -0400

    Update car-accidents.R

--- car-accidents.R
+++ car-accidents.R
@@ -7,9 +7,11 @@ library(gurobi)
 #setwd("/Users/Ahmad Dakhqan/Desktop/MSCI 433/Project/")
 setwd("/Users/Celeste/Documents/GitHub/car-accidents/")
 data=read.csv("data.csv")
-                         
+
 split = sample.split(data$C_SEV, SplitRatio = 0.7)
 dataTrain = subset(data, split == TRUE)   # Observations to be put in the training set           
 dataTest = subset(data, split == FALSE)  # Observations to be put in the testing set 
 nrow(dataTrain)
 nrow(dataTest)
+
+qualityLog = glm(C_SEV ~ P_AGE + P_SEX, data = dataTrain, family = binomial) 
